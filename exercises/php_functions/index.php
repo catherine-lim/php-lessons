6069cab12e76ab16c4fddab1733d3636416a3156
catherini168 <catherini168@gmail.com>
2019-08-20 02:13:05 +0000

    php-functions

---
+++ exercises/php_functions/index.php
@@ -0,0 +1,41 @@
+<?php
+function doMath( $num1, $num2, $op){
+    $num1 = floatval($num1);
+    $num2 = floatval($num2);
+    if($op==='+'){
+        return $num1+$num2;
+    } else if( $op === '-'){
+        return $num1-$num2;
+    } else if( $op === '*' || $op === 'x' || $op === 'X'){
+        return $num1 * $num2;
+    } else if( $op === '/'){
+        return $num1 / $num2;
+    }
+}
+
+$answer1 = doMath( 2, 4, '+');
+$answer2 = doMath( '2','4','*');
+print( "answer1: {$answer1} <br> answer2: {$answer2}");
+
+
+$people = ['Jake','Jane','Dirk','Dane'];
+
+function findPerson($name){
+    global $people;
+   $peopleLength = count($people);
+   $currentPosition = 0 ;
+   while( $currentPosition < $peopleLength && $people[$currentPosition]!==$name){
+       $currentPosition++;
+   }
+   if($currentPosition < $peopleLength){
+       return $currentPosition;
+   }
+   return -1;
+}
+$dirkPosition = findPerson( 'Dirk' );
+$karlPosition = findPerson( 'Karl' );
+print("<br>");
+print("Dirk was found at position {$dirkPosition} and Karl was found at position {$karlPosition}");
+
+
+?>
